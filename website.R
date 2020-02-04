c3e176dbded67b0d1a47062f56cfe234a3c14455
David Dobrowski <dobrowski@gmail.com>
2020-02-04 09:31:43 -0800

    init

--- website.R
+++ website.R
@@ -1,3 +1,10 @@
 
 
+# https://www.storybench.org/how-to-build-a-website-with-blogdown-in-r/
+
 library(tidyverse)
+library(blogdown)
+
+
+blogdown::new_site(dir = "aerial" , theme = "sethmacleod/aerial")
+
